a5a34f114b196c4f03c79552e78508cfa4ecc885
Madeline Cook <mcnadine.cook@gmail.com>
2018-05-13 20:04:23 -0700

    Added Locations tab and add loaction page

--- CBDBusRouting/CBDBusRouting/MainWindow.xaml.cs
+++ CBDBusRouting/CBDBusRouting/MainWindow.xaml.cs
@@ -20,10 +20,7 @@ namespace CBDBusRouting
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-             
-}
+        public MainWindow() => InitializeComponent();
+
     }
 }
