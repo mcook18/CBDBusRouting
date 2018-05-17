48bfc22e8daa226ab83401d8731f545a01f41ce3
Madeline Cook <mcnadine.cook@gmail.com>
2018-05-16 20:59:13 -0700

    Data classes for view

---
+++ CBDBusRouting/CBDBusRouting/DataClassesView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBDBusRouting
+{
+    class DataClassesView
+    {
+        class Group
+        {
+            public Location destination;
+            public int numStudents;
+
+            public Group(Location dest, int n)
+            {
+                destination = dest;
+                numStudents = n;
+            }
+        }
+
+        class Location
+        {
+            //might not be exhaustive
+            public string address;
+
+            public Location(string addr)
+            {
+                address = addr;
+            }
+        }
+
+        class Bus
+        {
+            public int numSeats;
+            public List<Group> groups;
+
+            public Bus(int n)
+            {
+                numSeats = n;
+                groups = new List<Group>();
+            }
+
+            public Bus(int n, List<Group> g)
+            {
+                numSeats = n;
+                groups = g;
+            }
+        }
+    }
+
+    
+}
